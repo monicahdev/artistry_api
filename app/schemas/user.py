3355545670b9b6f2c17a3bf30e06cbb68e6d0810
Monica Hernandez <monicahdev@gmail.com>
2025-11-22 15:04:29 +0100

    Created UserBase, UserCreate, and UserRead classes in user schema file

---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    role: str
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserRead(UserBase):
+    id: int
+    created_at: datetime | None = None
+
+class Config:
+    from_attributes = True  
