64a5f2b764594fcd31ede821e3254fcdd678f6e6
Monica Hernandez <monicahdev@gmail.com>
2025-11-23 19:16:39 +0100

    Created the base class, base, and config
    Created the base class and base in order to avoid circular import issue, and established the config for the app

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,6 @@
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from app.db.base_class import Base
+from app.models.booking import Booking  # noqa: F401
+from app.models.online_class import OnlineClass  # noqa: F401
+from app.models.service import Service  # noqa: F401
+from app.models.user import User  # noqa: F401
+from app.models.user_class_access import UserClassAccess  # noqa: F401
