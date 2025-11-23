b394fd98fafb27e82768e9521e36df2220e12ec7
Monica Hernandez <monicahdev@gmail.com>
2025-11-23 22:53:07 +0100

    Defined all-users endpoint logic

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,6 +1,11 @@
-from app.dependencies import get_current_user
+from typing import List
+
+from app.dependencies import get_current_admin, get_current_user
+from app.dependencies_auth import authenticate_user, create_user_token, get_db
 from app.models.user import User
+from app.schemas.user import UserRead
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -14,3 +19,10 @@ def get_profile(current_user: User = Depends(get_current_user)):
             "role": current_user.role,
         },
     }
+    
+@router.get("/all-users", response_model=list[UserRead])
+def get_all_users(
+    db: Session = Depends(get_db),
+    admin_user = Depends(get_current_admin),
+):
+    return db.query(User).all()
