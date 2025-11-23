b46ad69f68fa395ae2e3976ee6dc843fd76361af
Monica Hernandez <monicahdev@gmail.com>
2025-11-23 19:55:38 +0100

    Added APIRouter to auth.py
    Added register endpoint logic, as well as login endpoint logic

---
+++ app/api/routes/auth.py
@@ -0,0 +1,55 @@
+from app.dependencies_auth import authenticate_user, create_user_token, get_db
+from app.models.user import User
+from app.schemas.auth import Token, UserLogin, UserRegister
+from app.security import get_password_hash
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.post("/register", response_model=Token, status_code=status.HTTP_201_CREATED)
+def register_user(
+    user_in: UserRegister,
+    db: Session = Depends(get_db),
+):
+    # Check if email exists
+    existing = db.query(User).filter(User.email == user_in.email).first()
+    if existing:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with that email already exists",
+        )
+
+    user = User(
+        email=user_in.email,
+        password_hash=get_password_hash(user_in.password),
+        role="USER",
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    # Create token for user auth
+    access_token = create_user_token(user)
+    return Token(access_token=access_token)
+
+
+@router.post("/login", response_model=Token)
+def login(
+    credentials: UserLogin,
+    db: Session = Depends(get_db),
+):
+    user = authenticate_user(db, credentials.email, credentials.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Wrong email or password.",
+            headers={"Auth": "Bearer"},
+        )
+
+    access_token = create_user_token(user)
+    return Token(access_token=access_token)
