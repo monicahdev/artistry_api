34bda37a12860a8b0b9069fcb2dd3f45cc43ba53
Monica Hernandez <monicahdev@gmail.com>
2025-11-24 17:34:01 +0100

    Defined create booking endpoint logic

--- app/api/routes/bookings.py
+++ app/api/routes/bookings.py
@@ -1,3 +1,38 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.post("/", response_model=BookingRead, status_code=status.HTTP_201_CREATED)
+def create_booking(
+    booking_in: BookingCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    
+    if current_user.role != "USER":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only role USER can create bookings",
+        )
+
+    
+    service = db.query(Service).filter(Service.id == booking_in.service_id).first()
+    if not service:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Service not found",
+        )
+
+    booking = Booking(
+        user_id=current_user.id,
+        service_id=booking_in.service_id,
+        date_hour=booking_in.date_hour,
+        comments=booking_in.comments,
+        status="PENDING",
+    )
+
+    db.add(booking)
+    db.commit()
+    db.refresh(booking)
+
+    return booking
