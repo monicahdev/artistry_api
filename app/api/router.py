43cefa76e0df15e49f94a6f7dfea8b98e423337b
Monica Hernandez <monicahdev@gmail.com>
2025-11-24 12:38:47 +0100

    Updated general router w/ services_admin route

--- app/api/router.py
+++ app/api/router.py
@@ -1,4 +1,5 @@
-from app.api.routes import auth, bookings, classes, services, users
+from app.api.routes import (auth, bookings, classes, services, services_admin,
+                            users)
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -8,3 +9,4 @@ router.include_router(services.router, prefix="/services", tags=["services"])
 router.include_router(classes.router, prefix="/classes", tags=["classes"])
 router.include_router(bookings.router, prefix="/bookings", tags=["bookings"])
 router.include_router(users.router, prefix="/users", tags=["users"])
+router.include_router(services_admin.router, prefix="/admin/services", tags=["Admin - Services"]) 
