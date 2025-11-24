002862a4dadb96ed1bffdcb685ad43ad8c853291
Monica Hernandez <monicahdev@gmail.com>
2025-11-24 13:53:17 +0100

    Updated general routes tags

--- app/api/router.py
+++ app/api/router.py
@@ -4,9 +4,9 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-router.include_router(auth.router, prefix="/auth", tags=["auth"])
-router.include_router(services.router, prefix="/services", tags=["services"])
-router.include_router(classes.router, prefix="/classes", tags=["classes"])
-router.include_router(bookings.router, prefix="/bookings", tags=["bookings"])
-router.include_router(users.router, prefix="/users", tags=["users"])
-router.include_router(services_admin.router, prefix="/admin/services", tags=["Admin - Services"]) 
+router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+router.include_router(services.router, prefix="/services", tags=["Services"])
+router.include_router(classes.router, prefix="/classes", tags=["Classes"])
+router.include_router(bookings.router, prefix="/bookings", tags=["Bookings"])
+router.include_router(users.router, prefix="/users", tags=["Users"])
+router.include_router(services_admin.router, prefix="/admin/services", tags=["Admin - services"]) 
