78b89fcc93eb2644d844f0e8db8f85b6616dfb9a
Monica Hernandez <monicahdev@gmail.com>
2025-11-24 14:17:36 +0100

    Defined get individual service detail endpoint logic

--- app/api/routes/services.py
+++ app/api/routes/services.py
@@ -1,11 +1,28 @@
+from typing import List
+
 from app.dependencies import get_db
 from app.models.service import Service
-from fastapi import APIRouter, Depends
+from app.schemas.service import ServiceRead
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/", response_model=List[ServiceRead])
 def list_services(db: Session = Depends(get_db)):
     services = db.query(Service).all()
     return services
+
+@router.get("/{service_id}", response_model=ServiceRead)
+def get_service_detail(
+    service_id: int,
+    db: Session = Depends(get_db),
+):
+    
+    service = db.query(Service).filter(Service.id == service_id).first()
+    if not service:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Service not found",
+        )
+    return service
