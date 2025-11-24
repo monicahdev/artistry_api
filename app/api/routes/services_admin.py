08757b4c8e7d63910c78f3ac517c990d63029613
Monica Hernandez <monicahdev@gmail.com>
2025-11-24 12:39:26 +0100

    Defined create service endpoint logic

--- app/api/routes/services_admin.py
+++ app/api/routes/services_admin.py
@@ -1,13 +1,11 @@
 from app.dependencies import get_current_admin, get_db
 from app.models.service import Service
 from app.models.user import User
-from fastapi import APIRouter, Depends
+from app.schemas.service import ServiceCreate, ServiceRead
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 router = APIRouter()
-
-router = APIRouter(prefix="/admin/services", tags=["Admin - Services"])
-
 
 @router.get("/")
 def list_services_admin(
@@ -16,3 +14,15 @@ def list_services_admin(
 ):
     services = db.query(Service).all()
     return services
+
+@router.post("/", response_model=ServiceRead, status_code=status.HTTP_201_CREATED)
+def create_service(
+    service_in: ServiceCreate,
+    db: Session = Depends(get_db),
+    current_admin: User = Depends(get_current_admin),
+):
+    service = Service(**service_in.model_dump())
+    db.add(service)
+    db.commit()
+    db.refresh(service)
+    return service
