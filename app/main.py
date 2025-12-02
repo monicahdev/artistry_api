32dd04b01d69cb8cc88af2f493543314e713b415
Monica Hernandez <monicahdev@gmail.com>
2025-12-02 23:15:44 +0100

    Added origins
    Frontend localhost, for now

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.router import router as api_router
 from app.db import base
-from fastapi import FastAPI
 
 app = FastAPI(
     title="Artistry by Sara MUA",
+)
+
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
