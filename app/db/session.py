2e88fe84585ab9179e7998d1ee778f20f824d800
Monica Hernandez <monicahdev@gmail.com>
2025-12-04 20:48:22 +0100

    Modified database connection string
    Initialized db according to env (local/published)

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,19 @@
-from app.config import settings
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+from app.config import settings
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or settings.DATABASE_URL
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set.")
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
