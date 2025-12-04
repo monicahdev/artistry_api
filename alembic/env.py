bace5416bd182b1253ae1222ba683944357a4c29
Monica Hernandez <monicahdev@gmail.com>
2025-12-04 20:46:20 +0100

    Modified env of alemic
    Modification made to allow the use of system vars to set db connection config

--- alembic/env.py
+++ alembic/env.py
@@ -1,17 +1,26 @@
+import os
 from logging.config import fileConfig
+
+from sqlalchemy import create_engine, pool
 
 from alembic import context
 from app.config import settings
 from app.db.base import Base
-from sqlalchemy import create_engine, pool
 
 # Base Alembic config
 config = context.config
 
-# Use db URL from .env
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+# Load DATABASE_URL from environment (Render) or fallback to .env (local)
+DATABASE_URL = os.getenv("DATABASE_URL") or settings.DATABASE_URL
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set for Alembic.")
+
+# Tell Alembic what DB to use
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Logging
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
